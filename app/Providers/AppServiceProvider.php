505b028c35c64adf43991dbcd9dc260fe045c0a2
Just Playin <jp@justplayingames.com>
2017-07-08 11:22:48 -0500

    fixed issue with default string length too long for database keys

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +14,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        /**
+         * Fixes error
+         * [PDOException]
+         * SQLSTATE[42000]: Syntax error or access violation: 1071 Specified key was too long; max key length is 767 bytes
+         */
+        Schema::defaultStringLength(191);
     }
 
     /**
