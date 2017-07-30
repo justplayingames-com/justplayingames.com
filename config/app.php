1e1f8574cfb6ddff086b7715432709f84262f3b9
Just Playin <jp@justplayingames.com>
2017-07-30 11:23:27 -0500

    added justplayingames-game-canned-kitty

--- config/app.php
+++ config/app.php
@@ -169,6 +169,7 @@ return [
         Laravel\Tinker\TinkerServiceProvider::class,
         PragmaRX\Firewall\Vendor\Laravel\ServiceProvider::class,
         JustPlayinGames\Blog\BlogServiceProvider::class,
+        JustPlayinGames\Games\CannedKitty\ServiceProvider::class,
 
         /*
          * Application Service Providers...
